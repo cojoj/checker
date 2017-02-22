d24d0be4632dd5d779768d355fcd46966a990b0f
Mateusz Zając <zajac.mateusz@icloud.com>
2017-02-22 11:41:46 +0100

    Initial 🤗

---
+++ README.md
@@ -0,0 +1,23 @@
+# Checker
+
+Tool for **checking** (obviously) whether your computer is ready for **iOS development**.
+The main purpose of this script is to give it to **workshop attendees**, so you can peacefully jump straight to workshops without any additional hustle around their computers to check all necessary tools 🙌.
+
+Right now this script is checking:
+1. Presence of **Xcode**
+2. Version of **Xcode**
+3. Presence of **git**
+4. Version of **ruby**
+5. Presence of **CocoaPods**
+
+### Usage 🛠
+
+To simply run this command you have to:
+```bash
+curl -s https://github.com/cojoj/checker/checker.sh | sh
+```
+
+Of course you can clone repository, modify something and adjust this script to your needs.
+First step int customisation may be **versions** of software you want to check. So far there are only two of those:
++ MIN_XCODE_VERSION
++ MIN_RUBY_VERSION
---
+++ checker.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+MIN_XCODE_VERSION="8.2"
+MIN_RUBY_VERSION="2.0"
+
+vercomp () {
+	if [[ $1 == $2 ]]
+	then
+		return 0
+	fi
+	local IFS=.
+	local i ver1=($1) ver2=($2)
+	# fill empty fields in ver1 with zeros
+	for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+	do
+		ver1[i]=0
+	done
+	for ((i=0; i<${#ver1[@]}; i++))
+	do
+		if [[ -z ${ver2[i]} ]]
+		then
+			# fill empty fields in ver2 with zeros
+			ver2[i]=0
+		fi
+		if ((10#${ver1[i]} > 10#${ver2[i]}))
+		then
+			return 1
+		fi
+		if ((10#${ver1[i]} < 10#${ver2[i]}))
+		then
+			return 2
+		fi
+	done
+	return 0
+}
+
+check-xcode() {
+	xversion=$( xcodebuild -version | head -n1 | cut -d ' ' -f2 )
+	vercomp $MIN_XCODE_VERSION $xversion
+	if [ $? -ne 2 ] ; then
+		echo "The version of Xcode installed on your machine is too low. Please update to $MIN_XCODE_VERSION"
+		exit 1
+	fi
+	
+	return 0
+}
+
+check-ruby() {
+	ruby_version=$( ruby -v | cut -d ' ' -f2 | sed 's/.p//' )
+	vercomp $MIN_RUBY_VERSION $ruby_version
+	if [ $? -ne 2 ] ; then
+		echo "The version of Ruby installed on your machine is too low. Please update to $MIN_RUBY_VERSION"
+		exit 1
+	fi
+	
+	return 0
+}
+
+check-git() {
+	if ! [ -x "$(command -v git)" ] ; then
+		echo "You don't have git installed on your computer"
+		exit 1
+	fi
+	
+	return 0
+}
+
+check-cocoapods() {
+	if ! [ -x "$(command -v pod)" ] ; then
+		echo "You don't have CocoaPods installed on your computer"
+		echo "Please run this command: gem install cocoapods"
+		exit 1
+	fi
+		
+	return 0	
+}
+
+if 	type xcode-select >&- && 
+	xpath=$( xcode-select --print-path ) && 
+	test -d "${xpath}" && 
+	test -x "${xpath}" ; 
+then
+	check-xcode
+	check-git
+	check-ruby
+	check-cocoapods
+else
+	echo "Xcode isn't installed on your computer 😢"
+	exit 1
+fi
